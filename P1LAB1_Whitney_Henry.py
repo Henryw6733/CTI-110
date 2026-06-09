74b337602ff154f1de5b86593b1338e44ba3062e
Henryw6733 <henryw6733@student.faytechcc.edu>
2026-06-09 14:32:18 -0400

    First Python Assignment
    Taking input from user and printing output

---
+++ P1LAB1_Whitney_Henry.py
@@ -0,0 +1,9 @@
+# Whitney Henry
+# 06/09/2026
+# P1LAB1.py
+# input and print
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello,", first_name, last_name + "!","Welcome to CTI-110!")
